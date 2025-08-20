8c7958ca3ca588d9821f83b2bc00b03005d777e9
Kristina Hrynko <grnkkristina@gmail.com>
2025-08-20 09:02:46 +0300

    Map Version

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  Map<String, bool> questions = {
+    'You can lead a cow down stairs but not up stairs.': false,
+    'Approximately one quarter of human bones are in the feet.': true,
+    'A slug\'s blood is green.': true,
+  };
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions.keys.toList()[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,18 +60,29 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.all(15.0),
+              ),
               child: Text(
                 'True',
                 style: TextStyle(
-                  color: Colors.white,
                   fontSize: 20.0,
                 ),
               ),
               onPressed: () {
                 //The user picked true.
+                if (questions.values.toList()[questionNumber] == true) {
+                  print('yes');
+                } else {
+                  print('no');
+                }
+                setState(() {
+                  questionNumber ++;
+                  print(questionNumber);
+                });
               },
             ),
           ),
@@ -69,22 +90,36 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.all(15.0),
+              ),
               child: Text(
                 'False',
                 style: TextStyle(
                   fontSize: 20.0,
-                  color: Colors.white,
                 ),
               ),
               onPressed: () {
                 //The user picked false.
+                if (questions.values.toList()[questionNumber] == false) {
+                  print('yes');
+                } else {
+                  print('no');
+                }
+                setState(() {
+                  questionNumber ++;
+                  print(questionNumber);
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
